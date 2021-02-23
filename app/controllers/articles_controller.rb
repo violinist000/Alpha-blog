a1b671c1766e303f536f9c08b759c7ececa6687a
Mellissa White <mellissa0000@gmail.com>
2021-02-23 18:54:36 +0000

    Update and edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,11 +9,29 @@ class ArticlesController < ApplicationController
     end 
     
     def new
-        
+        @article = Article.new
     end 
     
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end 
     def create
-        render plain: params[:article]
-    end 
-    
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully!"
+            redirect_to @article
+        else 
+           render 'new'
+        end
+    end
 end
