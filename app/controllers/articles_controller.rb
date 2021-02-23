ba6837803041779dee2562140e1c0295ca010898
Mellissa White <mellissa0000@gmail.com>
2021-02-23 00:19:57 +0000

    add submit button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,13 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end 
+    
+    def new
+        
+    end 
+    
+    def create
+        render plain: params[:article]
+    end 
+    
 end
