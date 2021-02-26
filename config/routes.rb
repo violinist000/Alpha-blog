2e1b6d05732f77c85204d4bced596cc10b87457d
Mellissa White <mellissa0000@gmail.com>
2021-02-26 18:14:05 +0000

    home page styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
     
