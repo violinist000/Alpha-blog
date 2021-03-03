3b17e8e29dd649e75c50883a4812f2d993d0e082
Mellissa White <mellissa0000@gmail.com>
2021-03-03 03:56:45 +0000

    login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,20 @@ class SessionsController < ApplicationController
     end
     
     def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Logged in successfully"
+            redirect_to user
+        else
+            flash.now[:alert] = "Oops looks like you didn't put in the right information"
+            render 'new'
+        end
     end
     
     def destroy
+        session[:user_id] = nil
+        flash[:notice] = "You have left :'("
+        redirect_to root_path
     end
 end
