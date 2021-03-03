f8ec797a9e4c284e704c73f6e6b0fe9d10077fe8
Mellissa White <mellissa0000@gmail.com>
2021-03-03 18:41:29 +0000

    no sign up when logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
     
     def show
-       @user = User.find(params[:id]) 
        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
@@ -15,11 +15,9 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])
     end 
     
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information was successfully updated"
             redirect_to @user
@@ -32,6 +30,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome #{@user.username}, you have successfully arrived at intellectuality"
             redirect_to articles_path
         else 
@@ -43,4 +42,8 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+    
+    def set_user
+        @user = User.find(params[:id])
+    end 
 end 
