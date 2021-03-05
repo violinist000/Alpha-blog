47f97ad502623188a79991082f7cb23c4a41f7a3
Mellissa White <mellissa0000@gmail.com>
2021-03-05 16:37:03 +0000

    add categories table

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,17 @@
+class CategoriesController < ApplicationController
+    
+    def new
+        
+    end 
+    
+    def index
+        
+    end 
+    
+    def show
+        
+    end
+    
+    
+    
+end 
