1f08965238a24670b9d55787f36de4faf6ccfae5
Mellissa White <mellissa0000@gmail.com>
2021-03-05 21:27:28 +0000

    new category listing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,17 @@
 class CategoriesController < ApplicationController
     
     def new
-        
+        @category = Category.new
+    end 
+    
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category was successfully created"
+            redirect_to @category
+        else
+            render 'new'
+        end 
     end 
     
     def index
@@ -9,9 +19,13 @@ class CategoriesController < ApplicationController
     end 
     
     def show
-        
+        @category = Category.find(params[:id])
     end
     
+    private
     
+    def category_params
+        params.require(:category).permit(:name)
+    end 
     
 end 
