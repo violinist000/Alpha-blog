af877d898f87b8d374dbf6b1b0eb969664921363
Mellissa White <mellissa0000@gmail.com>
2021-03-09 16:12:57 +0000

    End of the application many-to-many associations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -59,7 +59,7 @@ class ArticlesController < ApplicationController
     
     
     def article_params
-       params.require(:article).permit(:title, :description)
+       params.require(:article).permit(:title, :description, category_ids: [])
     end
     
     def require_same_user
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,6 +15,21 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end 
+  
+  def updated
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+
+  end 
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -22,6 +37,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
   private
